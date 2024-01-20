1d6961e2ab20c9e53927b442d22607c3d3383f8d
Kevin Edwards <kle91203@gmail.com>
2024-01-19 21:06:23 -0700

    add daily expences

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,15 @@
 import './App.css'
 import RecuringExpences from './RecuringExpences'
+import DailyExpences from './DailyExpences'
 
 function App() {
 
   return (
     <>
       <RecuringExpences/>
+      <br/>
+      <br/>
+      <DailyExpences/>
     </>
   )
 }
