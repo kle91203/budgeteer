85887fb8430dbd0a6e7f6359a25bcf5963f54aab
Kevin Edwards <kle91203@gmail.com>
2024-01-19 20:48:55 -0700

    make it look like real expences

--- src/RecuringExpences.jsx
+++ src/RecuringExpences.jsx
@@ -1,9 +1,16 @@
 export default function RecuringExpences() {
 
-    const expences = [1,2,3]
+    const expences = [
+        {name: "electricity", amount: 10},
+        {name: "gas", amount: 20},
+        {name: "mortgage", amount: 100},
+        {name: "phones", amount: 40},
+        {name: "car loan", amount: 50},
+        {name: "water", amount: 30}
+    ]
 
     return (<>
-        {expences.map((i) => <div>{i}</div>)}
+        {expences.map((e) => <div key={e.name}>{e.name} {e.amount}</div>)}
     </>)
 
 }
